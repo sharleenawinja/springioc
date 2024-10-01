5b5a0f774a72a407fb1c0c70c5b8f82da1c63bf7
sharleenawinja <awinjasharleen@gmail.com>
2024-10-01 16:06:41 +0300

    create main method

---
+++ src/main/java/dev/hellospring/Application.java
@@ -0,0 +1,9 @@
+package dev.hellospring;
+
+import jdk.nashorn.internal.runtime.OptimisticReturnFilters;
+
+public class Application {
+    public static void main (String[] args ){
+        System.out.println("Hello spring");
+    }
+}
